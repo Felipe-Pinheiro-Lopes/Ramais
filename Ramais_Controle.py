466496cb3beeb43cfeee61be138f2692405da25d
Felipe Pinheiro Lopes <t_fplopes@prefeitura.local>
2023-05-03 15:56:30 -0300

    Adição de pesquisa Simples

--- Ramais_Controle.py
+++ Ramais_Controle.py
@@ -13,9 +13,22 @@ def funcao_principal():
     cursor.execute(f"INSERT INTO Ramais VALUES ('{nome}', '{telefone}', '{setor}', '{observacao}')")
     con.commit()
 
+def funcao_pesquisar():
+    ramal = formulario.lineEdit_5.text()
+    cursor.execute(f"SELECT * FROM Ramais WHERE nome='{ramal}'")
+    resultado = cursor.fetchone()
+    if resultado:
+        formulario.lineEdit_6.setText(resultado[0])
+        formulario.lineEdit_7.setText(resultado[1])
+        formulario.lineEdit_8.setText(resultado[2])
+        formulario.lineEdit_9.setText(resultado[3])
+    else:
+        QtWidgets.QMessageBox.warning(formulario, 'Aviso', 'Ramal não encontrado!')
+
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("untitled.ui")
 formulario.pushButton.clicked.connect(funcao_principal)
+formulario.pushButton_2.clicked.connect(funcao_pesquisar)
 
 formulario.show()
 app.exec()
